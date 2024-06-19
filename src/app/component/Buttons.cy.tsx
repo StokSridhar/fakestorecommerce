28e48c042b19677cbb614b55a6065db81c42b2c8
Sridhar <ronsridhar607@gmail.com>
2024-06-19 18:11:28 +0530

    Build Issue Fixed

--- src/app/component/Buttons.cy.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Buttons from './Buttons'
-
-describe('<Buttons />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<Buttons />)
-  })
-})
--- src/app/component/Checkbox.cy.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Checkbox from './Checkbox'
-
-describe('<Checkbox />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<Checkbox />)
-  })
-})
