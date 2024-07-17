113b08535287b4a9749cee677ae4918c2d0c8367
Sridhar <ronsridhar607@gmail.com>
2024-07-17 16:45:52 +0530

    Resume Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,21 @@
 import Promosection from "./components/promosection";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Testimonial from "./components/Testimonial";
+import Newsletter from "./components/Newsletter";
+import TopNotification from "./components/TopNotification";
+import Features from "./components/Features";
 
 export default function Home() { 
   
   return (
     <main> 
+      <TopNotification/>
       <Navbar/>
       <Promosection/> 
+      <Features/>
+      <Testimonial/>
+      <Newsletter/>
     <Footer/>
       </main>
   );
